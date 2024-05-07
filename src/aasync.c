3d86ed22427eabf780f5a6a0109dbc6ac65ea1d5
Jure Bagic <jurebagic@proton.me>
2024-05-07 04:57:35 +0200

    add licence and copyright notice to source files

--- src/aasync.c
+++ src/aasync.c
@@ -1,3 +1,19 @@
+/* ----------------------------------------------------------------------------------------------
+ * Copyright (C) 2023-2024 Jure Bagić
+ *
+ * This file is part of ashe.
+ * ashe is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * ashe is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with ashe.
+ * If not, see <https://www.gnu.org/licenses/>.
+ * ----------------------------------------------------------------------------------------------*/
+
 #include <signal.h>
 #include <stdio.h>
 
